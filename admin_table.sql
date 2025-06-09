af8008d867ab85463e5f5587576360e6968a21a6
shiva <udayveerdiwake2@r@gmailcom>
2025-06-09 17:28:35 +0530

    yes

--- admin_table.sql
+++ admin_table.sql
@@ -1,11 +1,5 @@
-CREATE DATABASE user_registrations;
-
-USE user_registrations;
-
-CREATE TABLE
-  users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  );
+CREATE TABLE users_register (    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
